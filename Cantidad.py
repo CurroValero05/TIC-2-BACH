95e050d107dcef3bbc444915ae4e97ada4a5b6d1
Curro <71649349+CurroValero05@users.noreply.github.com>
2020-11-07 17:13:54 +0100

    Add files via upload
    Ejercicio 11

---
+++ Cantidad.py
@@ -0,0 +1,11 @@
+def cantidad():
+    num=input ("Introudce un numero entero: ")
+    pal=raw_input ("Introduce una palabra: ")
+    if [num]==pal:
+        print ("El numero de letras es el mismo que el numero que has metido")
+
+
+
+
+
+cantidad ()
