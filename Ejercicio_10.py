2f20756fe36fd9137289c7a0c18661d76b10ef8a
Curro <71649349+CurroValero05@users.noreply.github.com>
2020-11-07 17:12:37 +0100

    Add files via upload
    3 números anteriores y 3 números posteriores

---
+++ Ejercicio_10.py
@@ -0,0 +1,12 @@
+def ejercicio_10():
+    num=input("Introduce un numero entero: ")
+    print "Los numeros anteriores son: ", num-1, num-2, num-3
+    print "Los siguientes numeros son: ", num+1, num+2, num+3
+    
+
+
+
+
+
+
+ejercicio_10 () 
