98db067c71814636b42f34423efb3a212f34450b
Curro <71649349+CurroValero05@users.noreply.github.com>
2020-11-07 17:15:54 +0100

    Add files via upload
    De $ a €

---
+++ Convertidor$a€.py
@@ -0,0 +1,8 @@
+# -*- coding: cp1252 -*-
+def convertidor():
+       dolares=input("Introduce una cantidad en dolares")
+       euros=dolares * 0.85
+       print(euros), "euros"
+
+convertidor()
+        
