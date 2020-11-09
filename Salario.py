f7aa77c3fdae5a86c5574b955902f091019d16b8
Curro <71649349+CurroValero05@users.noreply.github.com>
2020-11-09 13:34:46 +0100

    Add files via upload
    Ejercicio 9

---
+++ Salario.py
@@ -0,0 +1,10 @@
+def ejercicio_9():
+    horas_totales=input("Cuantas horas has hecho: ")
+    precio_normal=30
+    precio_extra=1.5*precio_normal
+    if(horas_totales<=40):
+        salario=horas_totales*precio_normal
+    else:
+        salario=40*precio_normal+(horas_totales-40)*precio_extra
+    print "Salario= ", salario
+ejercicio_9()
